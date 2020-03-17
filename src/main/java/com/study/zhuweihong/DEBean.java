fac333ae149dd22f7e960a1c0a42cc0e04fff3f0
杨攀 <473075889>
2020-03-17 17:56:46 +0800

    数字解析

---
+++ src/main/java/com/study/zhuweihong/DEBean.java
@@ -0,0 +1,4 @@
+package com.study.zhuweihong;
+
+public class DEBean {
+}
--- src/main/java/com/study/zhuweihong/PigDemo.java
+++ src/main/java/com/study/zhuweihong/PigDemo.java
@@ -8,34 +8,93 @@ public class PigDemo {
 
 
     public static void main(String[] args) {
-        System.out.println(parseDE("einhundertsechs", ""));
-        System.out.println(parseDE("zehnhunderteinhundert", ""));
 
-        List<String>  list = new ArrayList<>();
-        list.add("一");
-        list.add("二");
-        list.add("三");
-        list.add("四");
-        list.add("五");
-        list.add("六");
-        list.add("七");
-        list.add("八");
-        list.add("九");
-        list.add("十");
-        list.add("百");
-        list.add("千");
-//        list.add("");
-        Collections.sort(list);
-        System.out.println(list);
+//        System.out.println(parseDEToNum("einhundertsechs"));
+//        System.out.println(parseDEToNum("eintausendeinhundert"));
+//        System.out.println(parseDEToNum("einhundertdreizehn"));z
+        System.out.println(parseDEToNum("elftausendeinhundertelf"));
+        System.out.println(parseDEToNum("zwei"));
+
+
     }
 
+    public static int parseDEToNum(String str) {
+        List<Integer> list1 = new ArrayList<>(10);
+        List<Integer> list2 = new ArrayList<>(10);
+        parseDE(str, list1, list2);
+        List<Integer> list = new ArrayList<>(10);
+        changeToNum(list, list1, list2);
+        int result = 0;
+        for (Integer integer : list) {
+            result += integer;
+        }
+        return result;
+    }
+
+
+    public static void changeToNum(List<Integer> list, List<Integer> list1, List<Integer> list2) {
+        int maxBit = findMaxBit(list2);
+        if (maxBit == 1) {
+            if (list2.get(1) == 10000) {
+                //数字十几
+                list.add(list1.get(0) + 10);
+            } else {
+                //两百
+                int i = list1.get(0) * power(10, list2.get(0)) * power(10, list2.get(1));
+                list.add(i);
+            }
+        } else if (maxBit == 2) {
+            //数字53
+            int i = Integer.parseInt(list1.get(1) + "" + list1.get(0));
+            list.add(i);
+
+        } else if (maxBit == 0) {
+            //单个数字，例如35中的5,或者数字3，一百，一千
+            list.add(list1.get(0) * power(10, list2.get(0)));
+        }
+        if (list1.size() > maxBit + 1) {
+            list1 = list1.subList(maxBit + 1, list1.size());
+            list2 = list2.subList(maxBit + 1, list2.size());
+            changeToNum(list, list1, list2);
+        }
+    }
+
+    /**
+     * a的b次方
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int power(int a, int b) {
+        int power = 1;
+        for (int c = 0; c < b; c++)
+            power *= a;
+        return power;
+    }
+
+
+    public static int findMaxBit(List<Integer> list) {
+        if (list.size() == 0) {
+            return -1;
+        } else if (list.size() == 1) {
+            return 0;
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) {
+                return i - 1;
+            }
+        }
+        return 1;
+
+    }
 
     public static String parseDEResult(String str, int num) {
         if (str.length() > 1) {
             int maxBit = findMaxBit(str);
 
         }
-        return  null;
+        return null;
 
     }
 
@@ -43,7 +102,7 @@ public class PigDemo {
     public static int findMaxBit(String str) {
         String substring = str.substring(0, 1);
         String substring2 = str.substring(1, 2);
-            return 0;
+        return 0;
 
     }
 
@@ -134,11 +193,189 @@ public class PigDemo {
                     return result + "千";
                 }
                 return parseDE(str.substring(7), result + "千");
+            case "Mil":
+                if (str.equals("Million") || str.equals("Millionen")) {
+                    return result + "百万";
+                } else if (str.startsWith("Millionen")) {
+                    return parseDE(str.substring(9), result + "百万");
+                }
+                return parseDE(str.substring(7), result + "百万");
             default:
                 return result;
         }
 
     }
 
+    /**
+     * str 只能值德语的数字
+     *
+     * @param str
+     * @param result
+     * @return
+     */
+    public static void parseDE(String str, List<Integer> result, List<Integer> num) {
+        String temp = str.substring(0, 3);
+        switch (temp) {
+            case "und":
+                parseDE(str.substring(3), result, num);
+                break;
+            case "nul":
+                result.add(0);
+                num.add(0);
+                break;
+            case "ein":
+                result.add(1);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(3), result, num);
+                break;
+            case "zwe":
+                result.add(2);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "dre":
+                result.add(3);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "vie":
+                result.add(4);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "Fün":
+                result.add(5);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+            case "sec":
+                result.add(6);
+                num.add(0);
+                if (str.length() == 5) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "sie":
+                result.add(7);
+                num.add(0);
+                if (str.length() == 6) {
+                    return;
+                }
+                if (str.startsWith("sieben")) {
+                    parseDE(str.substring(6), result, num);
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "ach":
+                result.add(8);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "neu":
+                result.add(9);
+                num.add(0);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "zeh":
+                result.add(1);
+                num.add(10000);
+                if (str.length() == 4) {
+                    return;
+                }
+                parseDE(str.substring(4), result, num);
+                break;
+            case "elf":
+                result.add(11);
+                num.add(0);
+                if (str.length() == 3) {
+                    return;
+                }
+                parseDE(str.substring(3), result, num);
+                break;
+            case "Zwö":
+                result.add(12);
+                num.add(0);
+                if (str.length() == 5) {
+                    return;
+                }
+                parseDE(str.substring(5), result, num);
+                break;
+            case "zwa":
+                result.add(2);
+                num.add(1);
+                if (str.length() == 7) {
+                    return;
+                }
+                parseDE(str.substring(7), result, num);
+                break;
+            case "ßig":
+                result.add(1);
+                num.add(1);
+                if (str.length() == 3) {
+                    return;
+                }
+                parseDE(str.substring(3), result, num);
+                break;
+            case "zig":
+                result.add(1);
+                num.add(1);
+                if (str.length() == 3) {
+                    return;
+                }
+                parseDE(str.substring(3), result, num);
+                break;
+            case "hun":
+                result.add(1);
+                num.add(2);
+                if (str.length() == 7) {
+                    return;
+                }
+                parseDE(str.substring(7), result, num);
+                break;
+            case "tau":
+                result.add(1);
+                num.add(3);
+                if (str.length() == 7) {
+                    return;
+                }
+                parseDE(str.substring(7), result, num);
+                break;
+            case "Mil":
+                if (str.equals("Million") || str.equals("Millionen")) {
+                    result.add(1);
+                    num.add(6);
+                    return;
+                } else if (str.startsWith("Millionen")) {
+                    result.add(1);
+                    num.add(6);
+                    parseDE(str.substring(9), result, num);
+                }
+                parseDE(str.substring(7), result, num);
+                break;
+            default:
+                return;
+        }
 
+    }
 }
